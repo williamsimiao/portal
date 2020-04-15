fc9fdb082774d30a2053765823c71d1f12ddecb6
William Simiao <williamsimiao@gmail.com>
2020-04-15 19:21:30 -0300

    add validation to login

--- src/pages/AuthPages/LoginPage.vue
+++ src/pages/AuthPages/LoginPage.vue
@@ -8,23 +8,28 @@
             dark
             flat
           >
-            <v-toolbar-title>Login</v-toolbar-title>
+            <v-toolbar-title>{{ $tc('signin') }}</v-toolbar-title>
             <v-spacer />
           </v-toolbar>
           <v-card-text>
             <v-form>
               <v-text-field
+                :v-model="username"
                 :label="$tc('username')"
-                name="login"
                 prepend-icon="person"
-                type="text"
+                :error-messages="usernameErrors"
+                @input="$v.username.$touch()"
+                @blur="$v.username.$touch()"
               />
 
               <v-text-field
+                :v-model="password"
                 :label="$tc('password')"
-                name="password"
                 prepend-icon="lock"
                 type="password"
+                :error-messages="passwordErrors"
+                @input="$v.password.$touch()"
+                @blur="$v.password.$touch()"
               />
             </v-form>
           </v-card-text>
@@ -45,19 +50,46 @@ export default {
 
   }),
   computed: {
-
+    username: {
+      get () {
+        return this.$store.state.auth.username
+      },
+      set (val) {
+        return this.$store.commit('auth/SET_USERNAME', val)
+      }
+    },
+    password: {
+      get () {
+        return this.$store.state.auth.password
+      },
+      set (val) {
+        return this.$store.commit('auth/SET_PASSWORD', val)
+      }
+    },
+    // ERRORS
+    usernameErrors () {
+      const errors = []
+      if (!this.$v.username.$dirty) return errors
+      !this.$v.username.required && errors.push(this.$t('$validation.required'))
+      return errors
+    },
+    passwordErrors () {
+      const errors = []
+      if (!this.$v.password.$dirty) return errors
+      !this.$v.password.required && errors.push(this.$t('$validation.required'))
+      return errors
+    }
   },
   validations () {
     const validations = {}
     
-    const validEmail = value => {
-      if (!value) return true
-      let expression = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/g
-      return expression.test(value)
-    }
-    validations.email = {
+    // const validEmail = value => {
+    //   if (!value) return true
+    //   let expression = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})$/g
+    //   return expression.test(value)
+    // }
+    validations.username = {
       required,
-      validEmail
     },
     validations.password = {
       required
