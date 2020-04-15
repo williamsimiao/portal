90cac54d7d1147d7886322140cb0332a38b21a0e
William Simiao <williamsimiao@gmail.com>
2020-04-14 21:15:57 -0300

    add material icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import plugins from './plugins';
 import router from './router';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
